f79830425c71d39656d6396c1effe0fbe2c2cbd1
Darius Matthews <dmatthews@catalyte.io>
2021-04-05 11:35:13 -0500

    styled spinner to fit in card

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import spinner from "../../images/spinner.gif"
+import * as styles from "./Spinner.module.css"
 
 /**
  * @name Spinner
  * @returns a spinner gif when api calls are in process
  */
-const Spinner = () => <img src={spinner} alt="Loading..." />
+const Spinner = () => <img className={styles.spinner} src={spinner} alt="Loading..." />
 
 export default Spinner
